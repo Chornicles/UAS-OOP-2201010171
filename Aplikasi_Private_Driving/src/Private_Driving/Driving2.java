d2ad9754cbfd611c365cb90e646240b2456737ea
Chornicles <163788497+Chornicles@users.noreply.github.com>
2024-06-28 22:21:27 +0800

    Add files via upload

---
+++ Aplikasi_Private_Driving/src/Private_Driving/Driving2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Private_Driving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gung Ardi
+ */
+public class Driving2 {
+    public static List<Driving1> listDriving = new ArrayList();
+}
